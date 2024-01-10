c32f95ebba19db512b78a699fb45c882d3ca7e24
Ugi0 <90718205+Ugi0@users.noreply.github.com>
2024-01-10 17:33:25 +0200

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { FloatingImage } from './components/floatingimage.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <FloatingImage />
+    </div>
+  );
+}
+
+export default App;
